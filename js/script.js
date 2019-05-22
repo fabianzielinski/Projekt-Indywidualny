651fe658c50017b1a7fd4241d879c15a2b9c8a1f
fabianzielinski <fabz@o2.pl>
2019-05-22 18:28:22 +0200

    Add Statpage

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+'use strict';
 
+let menuBtn = document.querySelector('.main-side-bar-hamburger');
+let menuSideBar = document.querySelector('.main-side-bar');
+let menuUl = document.querySelector('ul');
+let menuLogo = document.querySelector('.main-logo');
+
+menuBtn.addEventListener('click', function() {
+    menuBtn.classList.toggle('open');
+    menuSideBar.classList.toggle('open');
+    menuUl.classList.toggle('open');
+    menuUl.classList.toggle('li');
+    menuLogo.classList.toggle('open');
+})
+
+// function toggleLogo() {
+//     if (menuLogo.classList.contains('open')) {
+//         menuLogo.classList.remove('open');
+//     } else {
+//         menuLogo.classList.add('open');
+//       }
+// }
